e5a33fc45244054a07e0d2e111997df7b916dfc5
aiden <aidenrphotos@gmail.com>
2023-03-08 10:08:00 -0500

    Add function to return path of file selected

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # img-to-ascii
 A Tkinter application that takes an image as input and returns an ascii version of it.
+| 11:50am | 7-3-23 |
--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from tkinter import *
+from tkinter import font
+from tkinter import filedialog as fd
 
 root = Tk()
 root.title('ITOA')
-root.geometry('300x300')
+root.geometry('700x700')
+
+def select_file():
+    filetypes = (
+        ('Image files', '*.jpg'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title = 'Open a file',
+        initialdir = '/',
+        filetypes = filetypes
+    )
+
+    print(filename)
+
+def print_file():
+    select_file()
+
+buttonFnt = font.Font(size=18)
+addFile = Button(root, text='Add Image', font=buttonFnt, command=print_file)
+addFile.place(x=275, y = 20)
+
 root.mainloop()
