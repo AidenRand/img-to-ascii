e05b293774ae4c30daf80f2f1c6ead1a6b1e9015
aiden <aidenrphotos@gmail.com>
2023-03-09 11:41:55 -0500

    Add view file button

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import font
 from tkinter import filedialog as fd
 import PIL.Image
+import webbrowser
 
 root = Tk()
 root.title('ITOA')
@@ -16,11 +17,16 @@ def select_file():
 
     filename = fd.askopenfilename(
         title = 'Open a file',
-        initialdir = '/',
+        initialdir = '/home',
         filetypes = filetypes
     )
 
     return filename
+
+# when view file button pressed open image in browser
+def open_image_file():
+    webbrowser.open('ascii_image.txt')
+
 
 def convert_img():
     file_path = select_file()
@@ -30,14 +36,16 @@ def convert_img():
     except:
         print(file_path, 'File not found')
 
+    # set the dimensions for the converted image
     width, height = img.size
     aspect_ratio = height/width
     new_width = 80
-    new_height = aspect_ratio * new_width * 0.55
+    new_height = aspect_ratio * new_width * 0.6
     img = img.resize((new_width, int(new_height)))
 
     img = img.convert('L')
 
+    # set the gray scale of the converted image
     characters = ['.', "'", '`', '^', '"', ',', ':', ';', 'I', 'l', '!',
                   'i', '>', '<', '~', '+', '_', '-', '?', '[', ']', '{',
                   '}', '1', '(', ')', '|', '\\', '/', 't', 'f', 'j', 'r',
@@ -59,17 +67,23 @@ def convert_img():
     with open("ascii_image.txt", 'w+') as f:
         f.write(ascii_image)
 
-    ls = Label(root, width=500, height=500, bg='#1e1e1e',
+    # create text box to get rid of previous ascii
+    reset_box = Label(root, width=500, height=500, bg='#1e1e1e',
                fg='white', highlightthickness=0 )
-    ls.place(x=-10, y=80)
-    # create tkinter text area to hold ascii
+    reset_box.place(x=-10, y=80)
+
+    # text area to hold ascii
     ascii_box = Text(root, width=80, height=aspect_ratio * new_width * 0.55, 
                      bg='#1e1e1e', fg='white', highlightthickness=0)
     ascii_box.place(x=150, y=80)
     ascii_box.insert(INSERT,  ascii_image)
 
-buttonFnt = font.Font(size=18)
-addFile = Button(root, text='Add Image', font=buttonFnt, command=convert_img)
-addFile.place(x=365, y=10)
+    view_file = Button(root, text='View File', font=button_font, command=open_image_file)
+    view_file.place(x=460, y=10)
+
+button_font = font.Font(size=18)
+
+add_file = Button(root, text='Add Image', font=button_font, command=convert_img)
+add_file.place(x=260, y=10)
 
 root.mainloop()
